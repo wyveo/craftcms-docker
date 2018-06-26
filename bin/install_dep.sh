17756a2947a4939a1e063984c00cfb0e2fcf2a73
Ted Kuo <ted@grana.com>
2018-06-26 11:51:39 +0800

    Automate installation of dependencies upon docker-compose. and installation of npm/bower/gulp in Dockerfile. Creation of craftcms-config ensure env/code merges well for configuration

--- bin/install_dep.sh
+++ bin/install_dep.sh
@@ -3,12 +3,15 @@
 ## This is a script that gels the environment with the code. It depends on the latest dependencies json(s) to
 ## generate the dependencies required.
 
+# The default shell is zsh, force loading of .bashrc by Docker, first to ensure npm is available.
+source /root/.bashrc
+
 # First, install all dependencies specified in composer.json
 cd /usr/share/nginx
 composer install
 
 # Second, trigger bower install to install style-guide (front-end) dependencies.
-bower isntall
+bower install
 
 # Install requirements for minification, js / css linting, and monitoring via gulp.
 npm install
